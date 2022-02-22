10e428abd9917809079fe4fb37afe30200ae6291
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2022-02-23 07:18:36 +0800

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -45,8 +45,8 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
         await callback_query.message.reply(
             "sɪʟᴀʜᴋᴀɴ ᴘɪʟɪʜ sᴛʀɪɴɢ ᴍᴀɴᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴋᴀᴍᴜ ᴀᴍʙɪʟ​",
             reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("ᴘʏʀᴏɢʀᴀᴍ​", callback_data="pyrogram"),
-                InlineKeyboardButton("ᴛᴇʟᴇᴛʜᴏɴ​", callback_data="telethon")
+                InlineKeyboardButton("★ ᴘʏʀᴏɢʀᴀᴍ​ ★", callback_data="pyrogram"),
+                InlineKeyboardButton("★ ᴛᴇʟᴇᴛʜᴏɴ ★​", callback_data="telethon")
             ]])
         )
     elif query in ["pyrogram", "telethon"]:
